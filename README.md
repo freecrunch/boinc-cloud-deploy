802befa197aa932a9e2332bed17ac35e0afe46b7
mpentler <tehhustler@hotmail.com>
2018-06-17 18:01:28 +0000

    first revision of script

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 <img src="https://pasteboard.co/images/HqcBMsm.png/download" width="60%" height="60%">
 # boinc-cloud-deploy
 Automatically deploy BOINC projects to freshly-deployed cloud server instances. Part of the FreeCrunch project: [https://freecrunch.github.io/](https://freecrunch.github.io/)
@@ -11,7 +12,14 @@ It will download the client software, download an optional command line GUI for 
 This isn't a container solution. Although the premise of containers can be useful for BOINC deployments in order to keep things simple this is a standard install to your actual server.
 
 # How to use
-Dead simple. Clone the repository, change to its directory, and run the included install.py script under sudo. Then follow the prompts.
+Dead simple. Clone the repository, change to its directory, and run the following:
+
+```
+chmod +x install.sh
+sudo install.sh
+```
+
+Then follow the prompts.
 
 # Contact
 Feel free to contact us via the details on our bio or post an issue.
---
+++ install.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# boinc-cloud-deploy v0.0.1
+# Part of the FreeCrunch project
+# https://freecrunch.github.io/
+
+# Setup and read user preferences
+install_boinctui="y"
+set_rpc_pw="y"
+
+echo "boinc-cloud-deploy v0.0.1"
+echo "https://freecrunch.github.io/"
+echo "--"
+
+echo "Please supply some credentials to use. These credentials must match the credentials you used to sign up for BAM."
+echo "Additionally the email address and password you supply will also be used when signing up for projects."
+echo ""
+read -p "Enter your username: " username
+echo -n "Enter your password: "
+read -s password
+echo
+
+while true; do
+    read -p "Install boinctui terminal GUI? [Y/n]: " yn
+    case $yn in
+        [Yy]* ) install_boinctui="y"; break;;
+        [Nn]* ) install_boinctui="n"; break;;
+        * ) echo "Y/N choice required";;
+    esac
+done
+
+while true; do
+    read -p "Set password for RPC access? [Y/n]: " yn
+    case $yn in
+        [Yy]* ) set_rpc_pw="y"; break;;
+        [Nn]* ) set_rpc_pw="n"; break;;
+        * ) echo "Y/N choice required";;
+    esac
+done
+
+echo "Beginning deployment..."
+
+# Upgrade system before deploying everything else
+apt-get update
+apt-get upgrade
+
+# Install boinc-client
+echo "Installing the BOINC client..."
+apt-get install boinc-client
+
+# Install boinctui (if required)
+if [ $install_boinctui == "y" ]; then
+    echo "Installing boinctui terminal GUI..."
+    apt-get install boinctui
+fi
+
+# Setup GUI RPC access (if required) with the supplied info
+if [ $set_rpc_pw == "y" ]; then
+    echo "Setting up RPC access and restarting the BOINC service. Remember to open inbound TCP port 31416 on your cloud instance."
+    echo $password >> /var/lib/boinc-client/gui_rpc_auth.cfg
+    systemctl restart boinc-client.service
+    sleep 1
+fi
+
+# Attach to BAM with the supplied credentials
+echo "Attaching BOINC to BAM..."
+boinccmd --acct_mgr attach https://bam.boincstats.com $username $passwors
+
+# Finished!
+echo "Finished!\n"
